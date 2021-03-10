2cd15597b1ccd25d3a3e22485852e4e63256dd11
1981697881 <1981697881@qq.com>
2021-03-10 20:29:42 +0800

    0.3.2

--- src/views/inspection/record/form/DetailT.vue
+++ src/views/inspection/record/form/DetailT.vue
@@ -51,6 +51,19 @@
           <img width="100%" :src="dialogImageUrl" alt="">
         </el-dialog>
       </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item :label="'签名'" >
+            <div class="demo-image__preview">
+              <el-image
+                style="width: 100px; height: 100px"
+                :src="url"
+                :preview-src-list="srcList">
+              </el-image>
+            </div>
+          </el-form-item>
+        </el-col>
+      </el-row>
     </el-form>
     <div slot="footer" style="text-align:center;padding-top: 10px">
       <el-button type="primary" @click="saveData('form')">保存</el-button>
@@ -76,6 +89,11 @@
         headers: {
           'authorization': getToken('insrx'),
         },
+        url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+        srcList: [
+          'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
+          'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg'
+        ],
         imgData: {
         },
         form: {
